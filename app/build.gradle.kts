747b23df31cf088f52cbc5989a33b1aeef6e8f61
Kaidong Hu <dongforregister@gmail.com>
2024-10-03 18:13:25 -0400

    Change package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.adaptive.layout.android)
-    implementation(libs.androidx.room.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
